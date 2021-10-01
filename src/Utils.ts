aa95d30517f08d2d6bf0a7f6065864a6b7a5c87b
SkyDonald <skydonald.dev@gmail.com>
2021-10-01 21:56:45 +0200

    added some functions

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,3 +1,6 @@
+import { Socket } from "socket.io";
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
+
 export default class Utils {
     private static POSIBILITIES = [1, 2, 3, 4, 5, 6];
 
@@ -7,5 +10,9 @@ export default class Utils {
             dices.push(this.POSIBILITIES[Math.floor(Math.random() * this.POSIBILITIES.length)]);
         }
         return dices;
+    }
+
+    public static isSocketValid(socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap>) {
+        return typeof socket.handshake.query.username === 'string';
     }
 }
