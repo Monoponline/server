41d841820ffde741ce917b0026d8012b2b99bf6a
SkyDonald <skydonald.dev@gmail.com>
2021-10-01 22:02:01 +0200

    added gulp and its dependencies (to compile typescript)

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const del = require('del');
+const gulp = require('gulp');
+const esbuild = require('gulp-esbuild');
+const { exec } = require('child_process');
+// const tsc = require('gulp-typescript');
+
+function clean () {
+  return del(['dist/**/*']);
+}
+
+function build () {
+  return gulp.src('src/**/*.ts')
+    .pipe(esbuild({
+      sourcemap: false,
+      format: 'cjs',
+      target: 'node12',
+      loader: {
+        '.ts': 'ts'
+      }
+    }))
+    .pipe(gulp.dest('dist'));
+}
+
+// function build () {
+//   return gulp.src('src/**/*.ts')
+//     .pipe(tsc('tsconfig.json'))
+//     .pipe(gulp.dest('dist'));
+// }
+
+function watch (cb) {
+  exec('nodemon dist/index', cb);
+  gulp.watch('src/**/*.ts', { delay: 500 }, gulp.series(clean, build));
+}
+
+exports.build = gulp.series(clean, build);
+exports.watch = gulp.series(clean, build, watch);
