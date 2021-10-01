640f39a38eacfce750a8b8c60e814f49d0c31762
SkyDonald <skydonald.dev@gmail.com>
2021-10-01 21:56:25 +0200

    upgraded Player class

--- src/Player.ts
+++ src/Player.ts
@@ -1,6 +1,11 @@
 export default class Player {
+    public static AVATARS = ['battleship', 'car', 'dog', 'hat', 'iron', 'shoe', 'thimble', 'wheelbarrow'];
+
     private name: string;
     private position: number;
+    private avatar: string;
+    private properties: number[];
+    private account: number;
 
     constructor(name: string) {
         this.name = name;
@@ -17,5 +22,39 @@ export default class Player {
 
     public setPosition(position: number) {
         this.position = position;
+    }
+
+    public getAccount() {
+        return this.account;
+    }
+
+    public setAccount(account: number) {
+        this.account = account;
+    }
+
+    public getAvatar() {
+        return this.avatar;
+    }
+
+    public setAvatar(avatar: string) {
+        this.avatar = avatar;
+    }
+
+    public getProperties() {
+        return this.properties;
+    }
+
+    public addProperty(property: number) {
+        this.properties.push(property);
+    }
+
+    public toJSON() {
+        return JSON.stringify({
+            name: this.name,
+            position: this.position,
+            avatar: this.avatar,
+            properties: this.properties,
+            account: this.account
+        });
     }
 }
